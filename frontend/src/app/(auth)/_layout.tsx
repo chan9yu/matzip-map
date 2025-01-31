72fdca7044384fe9b74dee5d63773fc39dffca9d
chan9yu <dev.cgyeo@gmail.com>
2025-01-31 07:47:55 -0800

    feat: 인증 네비게이션 스크린 옵션 구성

--- frontend/src/app/(auth)/_layout.tsx
+++ frontend/src/app/(auth)/_layout.tsx
@@ -2,10 +2,16 @@ import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
 	return (
-		<Stack>
-			<Stack.Screen name="index" />
-			<Stack.Screen name="signin" />
-			<Stack.Screen name="signup" />
+		<Stack
+			screenOptions={{
+				headerStyle: { backgroundColor: 'white' },
+				headerTitleStyle: { fontSize: 15 },
+				headerTintColor: 'black'
+			}}
+		>
+			<Stack.Screen name="index" options={{ headerTitle: '', headerShown: false }} />
+			<Stack.Screen name="signin" options={{ headerTitle: '로그인' }} />
+			<Stack.Screen name="signup" options={{ headerTitle: '회원가입' }} />
 		</Stack>
 	);
 }
--- frontend/src/app/(auth)/index.tsx
+++ frontend/src/app/(auth)/index.tsx
@@ -1,16 +1,13 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { Button, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
 	return (
 		<SafeAreaView style={styles.container} edges={['top', 'right', 'bottom', 'left']}>
-			<Text>Home Screen</Text>
 			<View>
-				<Link href={'/signin'}>로그인으로 이동</Link>
-			</View>
-			<View>
-				<Link href={'/signup'}>회원가입으로 이동</Link>
+				<Button title="로그인화면으로 이동" onPress={() => router.navigate('/signin')} />
+				<Button title="회원가입화면으로 이동" onPress={() => router.navigate('/signup')} />
 			</View>
 		</SafeAreaView>
 	);
